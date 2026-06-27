2e7236fc0c656e5ca983ee36f5fcfbd4b2d63a15
Arnaud Rebts <arnaud.rebts@gmail.com>
2026-06-27 10:02:14 +0200

    fix: set my password

--- modules/users/kid.nix
+++ modules/users/kid.nix
@@ -10,6 +10,7 @@
       users.users.kid = {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
+        hashedPassword = "$6$26CjWWzSY.6esMQn$BSmyaAyB9IlO5RxdtOL7q.0wQRDMc5P/PJ3to765u9Ll0PWQhvCmYlQksiaKgxyDi/9yzoo2LvjF7ubLQrpEQ0";
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAcnmLrPeTJeKsasfU0qn4sP4lBNeOUgRG4iZDS8nyEo kid@vulkan"
         ];
